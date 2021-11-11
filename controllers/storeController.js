02f6381de3a671ac03c21420a1ed8776654cb0dc
Claudiu Florin Popa <claudiup_15@yahoo.com>
2021-11-11 15:00:20 +0100

    Implemented aggregations

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -148,11 +148,16 @@ exports.heartStore = async (req, res) => {
     { new: true }
   );
   res.json(user);
-}
+};
 
 exports.getHearts = async (req, res) => {
   const stores = await Store.find({
     _id: { $in: req.user.hearts }
   });
   res.render('stores', { title: 'Hearted Stores', stores});
+};
+
+exports.getTopStores = async (req, res) => {
+  const stores = await Store.getTopStores();
+  res.render('topStores', { stores, title: '★ Top Stores' });
 }
